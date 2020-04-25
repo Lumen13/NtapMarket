e15b3dec266cf2a1bb614be63f4d7a89d9aeee70
lumen13 <ntap13@gmail.com>
2020-04-25 22:30:20 +0300

    Up

---
+++ NtapMarket.Data.EF/DesignTimeDbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NtapMarket.Data.EF
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<DBContext>
+    {
+        private string _connectionString 
+        { 
+            get 
+            { 
+                return "data source=localhost;initial catalog=NtapMarket;Integrated Security=True;";
+            }
+        }
+
+        public DBContext CreateDbContext(string[] args)
+        {
+            return new DBContext(_connectionString);
+        }
+    }
+}
---
+++ NtapMarket.Web.Seller/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NtapMarket.Data.IRepository;
+using NtapMarket.Web.Seller.Models;
+
+namespace NtapMarket.Web.Seller.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IProductModelRepository _productModelRepository;
+        private const int sellerId = 1;
+
+        public HomeController(ILogger<HomeController> logger
+            , IProductModelRepository productModelRepository)
+        {
+            _productModelRepository = productModelRepository;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var productModels = _productModelRepository.GetProductModel(sellerId);
+            return View(productModels);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
