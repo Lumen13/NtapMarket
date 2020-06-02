b87a0a9dbf4c8652df8889ef07747fc1ce1c7063
lumen13 <ntap13@gmail.com>
2020-06-02 09:40:25 +0300

    Ready: 1. All images from DB positioning in column with a pritty Delete buttons. Works fine 2. All User's images positioning in column with a pritty Delete buttons. Works fine 3. JS code looks good and work correctly
    Tasks:
    1. Make correct output fil source into the c#

--- NtapMarket.Web.Seller/wwwroot/js/site.js
+++ NtapMarket.Web.Seller/wwwroot/js/site.js
@@ -1,35 +1,60 @@
-﻿var EditProduct = function (e) {
-    var files = e.target.files;
-    var formData = new FormData();
+﻿function creater() {
+    let inputClickCounter = 0
+    function upload(element) {
+        let file = element.files[0]
+        const reader = new FileReader()
 
-    for (var i = 0; i < files.length; i++) {
+        reader.onloadend = loadImg
+        function loadImg() {
+            const button = document.createElement('button')
+            const selector = `userDiv${inputClickCounter}`
+            button.innerText = 'X'
+            button.className = 'btn btn-danger'
+            button.style.position = 'absolute'
+            button.style.opacity = 0.7
+            button.onclick = function () {
+                const sl = `.${selector}`
+                document.querySelector(sl).remove()
+            }
 
-        //var keyName = `productModel.productImage${i}.imageFile`;
-        //formData.append(keyName, files[i]);
-        
-        //elem.insertAdjacentHTML('beforebegin', ``)
-    }    
+            const img = document.createElement('img')
+            img.style.height = '400px'
+            img.style.width = '400px'
+            img.src = reader.result
 
-    //axios.post("Edit", formData);
-}
+            const wraper = document.createElement('div')
+            const wraperClass = selector
+            wraper.classList.add(wraperClass)
 
-function readURL(input) {
-    if (input.files && input.files[0]) {
-        var reader = new FileReader();
+            wraper.appendChild(button)
+            wraper.appendChild(img)
 
-        reader.onload = function (e) {
-            $('#myImage')
-                .attr('src', e.target.result);
-        };
-
-        reader.readAsDataURL(input.files[0]);
+            document.querySelector('.userImages').appendChild(wraper)
+        }
+        inputClickCounter++
+        reader.readAsDataURL(file)
     }
+    return upload
 }
+
+const imageUpload = creater()
 
 var DeleteImage = function (i) {
     for (var j = 0; j < document.body.children[1].firstElementChild.firstElementChild.children.length; j++) {
-        if (document.body.children[1].firstElementChild.firstElementChild.children[j].className == i) {
-            document.body.children[1].firstElementChild.firstElementChild.children[j].remove()
-        } 
-    }    
+        if (document.body.children[1].firstElementChild.firstElementChild.children[4].children[j].className === `dbImg-${i}`) {
+            document.body.children[1].firstElementChild.firstElementChild.children[4].children[j].remove()
+        }
+    }
 }
+
+//var EditProduct = function (e) {
+//    var files = e.target.files;
+//    var formData = new FormData();
+
+//    for (var i = 0; i < files.length; i++) {
+//        var keyName = `productModel.productImage${i}.imageFile`;
+//        formData.append(keyName, files[i]);
+//    }
+
+//    axios.post("Edit", formData);
+//}
